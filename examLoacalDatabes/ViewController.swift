8f125219373ae9a7b740007cbc1f5358ad8b8f40
Kishan Govindiya <kishangovindiya1@gmail.com>
2023-04-12 11:09:56 +0530

    Initial Commit

---
+++ examLoacalDatabes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  examLoacalDatabes
+//
+//  Created by R82 on 12/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
